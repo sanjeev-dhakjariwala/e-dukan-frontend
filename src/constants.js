18a5e61f2fb67cfb1e5bb325e074a3c5ba79c767
sanjeev-dhakjariwala <sanjeevjha882@gmail.com>
2023-06-02 01:02:17 +0530

    minor

--- src/constants.js
+++ src/constants.js
@@ -1,6 +1,6 @@
 // export const BASE_URL =
-//   process.env.NODE_ENV === 'develeopment' ? 'http://localhost:5000' : '';
-export const BASE_URL = 'http://localhost:3001'; // If using proxy
+//   process.env.ENVIRONMENT === 'development' ? 'http://localhost:3001' : 'http://35.153.52.62:3001';
+export const BASE_URL = 'https://e-dukan-backend-production.up.railway.app/'; // If using proxy
 export const PRODUCTS_URL = '/api/products';
 export const USERS_URL = '/api/users';
 export const ORDERS_URL = '/api/orders';
