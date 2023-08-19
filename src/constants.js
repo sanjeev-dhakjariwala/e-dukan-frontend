4711a6d047f646ac4b7ccb659fe3d68e27f6fc74
sanjeev-dhakjariwala <sanjeevjha882@gmail.com>
2023-08-19 13:01:58 +0530

    BASE_URL removed from constants

--- src/constants.js
+++ src/constants.js
@@ -1,6 +1,5 @@
 // export const BASE_URL =
 //   process.env.ENVIRONMENT === 'development' ? 'http://localhost:3001' : 'http://35.153.52.62:3001';
-export const BASE_URL = 'https://e-dukan-backend-production.up.railway.app/'; // If using proxy
 export const PRODUCTS_URL = '/api/products';
 export const USERS_URL = '/api/users';
 export const ORDERS_URL = '/api/orders';
--- src/slices/apiSlice.js
+++ src/slices/apiSlice.js
@@ -1,7 +1,6 @@
 import { fetchBaseQuery, createApi } from '@reduxjs/toolkit/query/react';
-import { BASE_URL } from '../constants';
 
-const baseQuery = fetchBaseQuery({ baseUrl: BASE_URL });
+const baseQuery = fetchBaseQuery({ baseUrl: process.env.REACT_APP_BASE_URL });
 
 export const apiSlice = createApi({
   baseQuery,
