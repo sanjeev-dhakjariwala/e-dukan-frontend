c6d4f265f64981b1e5857cc6bc32653a7b319f94
sanjeev-dhakjariwala <sanjeevjha882@gmail.com>
2023-08-19 13:04:18 +0530

    title changed

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -34,7 +34,7 @@ const Header = () => {
           <LinkContainer to='/'>
             <Navbar.Brand>
               {/* <img src={logo} alt='eDukan' /> */}
-              eDukan
+              ShopKart
             </Navbar.Brand>
           </LinkContainer>
           <Navbar.Toggle aria-controls='basic-navbar-nav' />
